4561d26abafa8f3102ddc2ccc24ad8b86a78a736
zkbenny <benny@zklink.org>
2022-07-30 16:15:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+Study rust, day day up.
+
+## hello-rust
+
+An example open the world of `rust`.
+
+## adder
+
+A simple `lib` example with unit tests.
+
+## guessing_game
+
+An example with input from `stdin`.
+
+## minigrep
+
+An example with `mod`,
---
+++ minigrep/src/main.rs
@@ -0,0 +1,25 @@
+mod lib;
+
+use std::env;
+use std::process;
+use lib::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = lib::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+
